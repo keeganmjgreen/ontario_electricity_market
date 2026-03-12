99363c34c6c6ba868cc6bd9396554bb7b6e129e8
Keegan Green <keeganmjgreen@gmail.com>
2026-03-11 23:36:24 -0400

    `uv run jupyter` in `build_book.sh`

--- build_book.sh
+++ build_book.sh
@@ -1,5 +1,5 @@
 gsed -i 's/:width: 64%/:width: 100% /' 2_the_supply_and_demand_of_electricity.md
-jupyter book build --pdf
+uv run jupyter book build --pdf
 gsed -i 's/:width: 100% /:width: 64%/' 2_the_supply_and_demand_of_electricity.md
 
 pdftoppm -f 1 -l 1 -png how_electricity_markets_work.pdf > img/pdf_page_1.png
